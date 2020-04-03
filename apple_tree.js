df74e2b3a17a72861721e84d3b22884c78b0a536
Sujackson <su_jackson99@yahoo.com>
2020-04-03 22:18:26 +0700

    done

---
+++ apple_tree.js
@@ -0,0 +1,68 @@
+class FruitTree {
+constructor (name, age, height,mature, healthStatus=true) {
+    this._name=name
+    this._age=age
+    this._height=height
+    this._mature=mature
+    this._healthStatus=healthStatus
+    this._random=Math.floor(Math.random()*20)+2
+    this._die=Math.floor(Math.random()*20)+2
+    
+}
+get name(){
+    return this._name
+}
+get age () {
+    return this._age
+}
+    
+get height () {
+    return this._height
+}
+
+get mature (){
+    return this._mature
+}
+
+get healthStatus () {
+    return this._healthStatus
+}
+
+get random(){
+    return this._random
+}
+
+get die(){
+    return this._die
+}
+
+}
+
+class AppleTree extends FruitTree{
+
+// Initialize a new MangoTree
+constructor (name, age, height,mature, healthStatus ,die=Math.floor(Math.random()*20)+2) {
+    super(name, age, height,mature, healthStatus)
+}
+
+// Get current states here
+
+// Grow the tree
+grow () {
+    // this._height=0
+    if(this.healthStatus==true){     
+
+        this._age+=1
+
+        if(this._height<=this._random){
+            this._height+=(Math.random()*50)
+            this._height= +this._height.toFixed(2)
+        }
+        if((this._age)>=this.mature+10){
+            this._healthStatus=false
+        }
+    }
+}
+}
+
+module.exports=AppleTree
--- index.js
+++ index.js
@@ -1,8 +1,55 @@
-const MangoTree = require('./mango_tree.js');
-const AppleTree = require('./apple_tree.js');
-const PearTree = require('./pear_tree.js');
+const MangoTree = require('./mango_tree');
+const AppleTree = require('./apple_tree');
+const PearTree = require('./pear_tree');
 
-class TreeGrove {}
+
+class TreeGrove {
+constructor(){
+    this.list=[]
+}
+
+inputTree(name, age, height, mature, healthStatus){
+    if(name==='MangoTree'){
+        this.list.push(new MangoTree(name,age, height, mature, healthStatus))
+    }else if(name==='AppleTree'){
+        this.list.push(new AppleTree(name,age, height, mature, healthStatus))
+    }else if(name==='PearTree'){
+        this.list.push(new PearTree(name,age, height, mature, healthStatus))
+    }
+    return this
+}
+
+nextYear(){
+    for (let j = 0; j <=10; j++) {
+        for (let i = 0; i < this.list.length; i++) {
+            this.list[i].grow()
+        }
+    }
+}
+showAges(){
+    for (let i = 0; i < this.list.length; i++) {
+        console.log(`${this.list[i].name} | age : ${this.list[i].age} years`)
+    }
+}
+showTrees(){
+    for (let i = 0; i < this.list.length; i++) {
+        console.log(`${this.list[i].name} | height : ${this.list[i].height} | age : ${this.list[i].age} | mature: ${this.list[i].mature} | health status : ${this.list[i].healthStatus} `)
+    }
+}
+showMatureTrees(){
+    for (let i = 0; i < this.list.length; i++) {
+        console.log(`${this.list[i].name} | height : ${this.list[i].height} | age : ${this.list[i].age} | mature age: ${this.list[i].mature} | health status: ${this.list[i].healthStatus}`)
+    }
+
+}
+showDeadTrees(){
+    for(let i = 0; i < this.list.length; i++){
+        if(this.list[i].healthStatus == false){
+            console.log(`${this.list[i].name} | height : ${this.list[i].height} | age : ${this.list[i].age} | mature age: ${this.list[i].mature} | health status: ${this.list[i].healthStatus}`)
+        }
+    }
+}
+}
 
 var grove = new TreeGrove()
 // input your trees data !
@@ -15,18 +62,21 @@ grove.inputTree("MangoTree", 3, 1.8, 7, true)
 grove.inputTree("MangoTree", 5, 2.4, 12 ,true)
 grove.inputTree("AppleTree", 4, 1.2, 5, true)
 grove.inputTree("PearTree", 7, 2, 15, true)
-
 // next year
+
 grove.nextYear()
 
 // show trees ages
 grove.showAges()
 
 // show trees
+console.log('--------------------- All Trees --------------------')
 grove.showTrees()
 
 // show trees
+console.log('-------------------- Mature Trees --------------------')
 grove.showMatureTrees()
 
 // show trees
+console.log('-------------------- Dead Trees --------------------')
 grove.showDeadTrees()
