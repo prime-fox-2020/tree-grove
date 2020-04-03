ef41cbf74085dade413e58c1d3595ee6c264c926
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-04-03 22:59:38 +0700

    tugas tree groove

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 const MangoTree = require('./mango_tree.js');
 const AppleTree = require('./apple_tree.js');
 const PearTree = require('./pear_tree.js');
+// const Test = require('./tes.js')
 
-class TreeGrove {}
+class TreeGrove {
+    constructor (){
+        this.trees = []
+    }
 
+    inputTree(name, age, height, harvestAge, healthStatus) {
+        if(name === 'MangoTree') {
+            this.trees.push(new MangoTree(age, height, harvestAge, healthStatus))
+        }
+        else if(name === 'AppleTree') {
+            this.trees.push(new AppleTree(age, height, harvestAge, healthStatus))
+        }
+        else if(name === 'PearTree') {
+            this.trees.push(new PearTree(age, height, harvestAge, healthStatus))
+        }
+    }
+
+    nextYear() {
+        for (let i = 0; i < this.trees.length; i++) {
+            this.trees[i].grow();
+        }
+        return this;
+    }
+
+    showAges() {
+        for (let i = 0; i < this.trees.length; i++) {
+            console.log(`${this.trees[i].name} | age : ${this.trees[i].age}`)
+        }
+        return this;
+    }
+
+    showTrees() {
+        for (let i = 0; i < this.trees.length; i++) {
+            console.log(`${this.trees[i].name} | height : ${this.trees[i].height} | age : ${this.trees[i].age} | mature age : ${this.trees[i].harvestAge} | health status : ${this.trees[i].healthStatus}`)
+        }
+        return this;
+    }
+
+    showMatureTrees() {
+        for (let i = 0; i < this.trees.length; i++) {
+            if (this.trees[i].age >= this.trees[i].harvestAge) {
+                console.log(`${this.trees[i].name} | height : ${this.trees[i].height} | ${this.trees[i].age} | mature age : ${this.trees[i].harvestAge} | health status : ${this.trees[i].healthStatus}`)
+            }
+        }
+        return this;
+    }
+
+    showDeadTrees() {
+        for (let i = 0; i < this.trees.length; i++) {
+            if (this.trees[i].healthStatus === false) {
+                console.log(`${this.trees[i].name} | height : ${this.trees[i].height} | ${this.trees[i].age} | mature age : ${this.trees[i].harvestAge} | health status : ${this.trees[i].healthStatus}`)
+            }
+        }
+    }
+
+}
 var grove = new TreeGrove()
 // input your trees data !
 // parameter ke-1: nama pohon
@@ -11,10 +66,10 @@ var grove = new TreeGrove()
 // pamareter ke-3: tinggi pohon pertama kali ketika ditanam di taman tersebut
 // parameter ke-4: umur mature pohon tersebut
 // parameter ke-5: healthStatus dari pohon tersebut ketika ditanam
-grove.inputTree("MangoTree", 3, 1.8, 7, true)
-grove.inputTree("MangoTree", 5, 2.4, 12 ,true)
-grove.inputTree("AppleTree", 4, 1.2, 5, true)
-grove.inputTree("PearTree", 7, 2, 15, true)
+grove.inputTree("MangoTree", 13, 1.8, 7, true)
+grove.inputTree("MangoTree", 15, 2.4, 12 ,true)
+grove.inputTree("AppleTree", 14, 1.2, 5, true)
+grove.inputTree("PearTree", 17, 2, 15, true)
 
 // next year
 grove.nextYear()
@@ -24,9 +79,10 @@ grove.showAges()
 
 // show trees
 grove.showTrees()
+// console.log(grove.trees)
 
-// show trees
+// // show trees
 grove.showMatureTrees()
 
-// show trees
+// // show trees
 grove.showDeadTrees()
---
+++ pear_tree.js
@@ -0,0 +1,53 @@
+class FruitTree {
+  constructor (name, age, height, harvestAge, healthStatus) {
+    this._name = name;
+    this._age = age;
+    this._height = height;
+    this._harvestAge = harvestAge;
+    this._healthStatus = healthStatus;
+    this._maxAge = 5 + harvestAge;
+  }
+
+  get name () {
+    return this._name;
+  }
+  
+  get age () {
+    return this._age;
+  }
+
+  get height () {
+    return this._height;
+  }
+
+  get healthStatus () {
+    return this._healthStatus;
+  }
+
+  get harvestAge () {
+    return this._harvestAge;
+  }
+
+  // Get current states here
+
+  // Grow the tree
+  grow () {
+    if (this._healthStatus) {
+      this._age += 1;
+      this._height += Math.random();
+      this._height = Number(Number(this._height).toFixed(2));
+      if (this._age >= this._maxAge) {
+        this._healthStatus = false;
+      }
+    }
+    return this;
+  }
+}
+
+class PearTree extends FruitTree{
+  constructor(age, height, matureAge, healtyStatus){
+      super('Pear Tree', age, height, matureAge, healtyStatus )
+  }
+}
+
+module.exports = PearTree;
